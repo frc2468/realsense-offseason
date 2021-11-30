ec9e347822d9fc88a11a5c95f6a0ccc796644b79
Christian Flewelling <cflew@tutanota.com>
2021-11-30 17:13:41 -0600

    Odometry testing script

---
+++ rpi/src/odom-test.py
@@ -0,0 +1,21 @@
+import pyrealsense2 as rs2
+
+def main():
+    pipe = rs2.pipeline()
+
+    pipe.start()
+    try:
+        while True:
+            frames = pipe.wait_for_frames()
+            pose = frames.get_pose_frame()
+            if pose:
+                data = pose.get_pose_data()
+                print('Frame num: {}'.format(pose.frame_number))
+                print('Position: {}'.format(data.translation))
+    finally:
+        pipe.stop()
+
+
+
+if __name__ == '__main__':
+    main()
