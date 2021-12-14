c03b52734c0ef5cc2924a53f7dec08598bb5dbc9
Christian Flewelling <cflew@tutanota.com>
2021-12-13 20:39:56 -0600

    Revert "Update LICENSE again?" and other stuff?
    I am small brain so I forgot to revert the commit before making this new
    one. This adds clion stuff and I moved some files
    
    This reverts commit 1cc6ea5d476d049881cbc5dd9cea186414004702.

--- rpi/src/main.cpp
+++ rpi/src/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <librealsense2/rs.hpp>
 
-int main() {
-    std::cout << "hello world" << std::endl;   
+
+int main(int argc, char * argv[])try {
+    rs2::pipeline pipe;
+    rs2::config cfg;
+    cfg.enable_stream(RS2_STREAM_POSE);
+    pipe.start(cfg);
+
+    while(true) {
+        rs2::frameset frames = pipe.wait_for_frames();
+        rs2::frame frame = frames.first_or_default(RS2_STREAM_POSE);
+        rs2_pose pose_sample = frame.as<rs2::pose_frame>().get_pose_data();
+        std::cout << "position (" << pose_sample.translation.x << ", " << pose_sample.translation.y << ", " << pose_sample.translation.z << std::endl;
+    }
+
     return 0;
+} catch (const rs2::error & e) {
+    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
+    return -1;
+} catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return -1;
 }
-
